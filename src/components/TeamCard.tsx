ef8f7712caf4446ef30134f10057cb51061136df
ackenney <ackenney@gmail.com>
2025-04-03 17:48:28 -0400

    fix team grid

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -13,14 +13,14 @@ export const TeamCard = ({
 }: TeamCardProps) => {
   return (
     <div className="flex flex-col gap-1 bg-white p-4 rounded-xl w-full max-w-96">
-      <div className="rounded-xl  w-full overflow-hidden">
-        <img src={imageUrl} className="h-full  object-cover" />
+      <div className="rounded-xl h-56 w-full overflow-hidden">
+        <img src={imageUrl} className="h-full w-40 object-cover" />
       </div>
       <div className="flex flex-col gap-1">
         <span className="text-sm font-bold">
           {title}
         </span>
-        <p className="text-sm leading-5">
+        <p className="text-sm leading-1">
           {children}
         </p>
       </div>
