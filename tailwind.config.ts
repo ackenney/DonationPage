f9c2262762f4a997299ae355524a439cf2484fa3
ackenney <ackenney@gmail.com>
2025-04-10 14:29:26 -0400

    add color package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,19 @@ export default {
         'color1': '#ff7849',
         'color2': '#969696',
         'TextColor1':'#525252',
-        'TextColor2':'#000000'
+        'TextColor2':'#000000',
+        
+        'salmon': '#F2655A',
+        'navy': '#293954',
+        'green':'#6AA870',
+        'lightBlue':'#88D2FF',
+        'NearBlack':'#0D190F',
+        'OffWhite':'#FFFFF5',
+        
+
+
+
+
       },
       fontFamily: {
         sans: ["Playfair", ...defaultTheme.fontFamily.sans],
