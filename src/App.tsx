7961d1083db2900cf7573f932684c959fb8e3821
ackenney <ackenney@gmail.com>
2025-04-23 16:09:58 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import Home from "./pages/Home";
 import { Footer } from "./components/layouts/Footer";
 import { Header } from "./components/layouts/Header";
 
+const fromEmail = import.meta.env.VITE_FROM_EMAIL;
+
 export default function App() {
   return (
     <>
       <Header />
       <Home />
-      <p>{import.meta.env.VITE_API_URL}</p>
+      <p>{fromEmail}</p>
       <Footer />
     </>
   );
