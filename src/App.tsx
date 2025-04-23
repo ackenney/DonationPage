b76ec6330f772b731687e88a8b1f0a8043c5938c
ackenney <ackenney@gmail.com>
2025-04-23 16:14:05 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import Home from "./pages/Home";
 import { Footer } from "./components/layouts/Footer";
 import { Header } from "./components/layouts/Header";
 
-const fromEmail = import.meta.env.VITE_FROM_EMAIL;
 
 export default function App() {
   return (
     <>
       <Header />
       <Home />
-      <p>{fromEmail}</p>
+
       <Footer />
     </>
   );
