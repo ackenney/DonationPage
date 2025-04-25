a2a8415507bb061c207e4c57e51594be2b495fd8
ackenney <ackenney@gmail.com>
2025-04-25 15:39:17 -0400

    test

--- src/pages/sections/AboutUsSection.tsx
+++ src/pages/sections/AboutUsSection.tsx
@@ -10,7 +10,7 @@ export default function AboutUsSection() {
           className="rounded-3xl w-full"
           src={img} alt="img2" /> 
         <div className="flex flex-col items-start">
-          <span className="text-xs lg:text-sm text-navy tracking-widest">ABOUT US</span>
+          <span className="text-xs lg:text-sm text-navy tracking-widest">ABOUTs US</span>
           <h2 className="text-3xl lg:text-5xl text-salmon font-bold">Maroon Strategy</h2>
           <p className="py-4 leading-loose text-nearBlack lg:leading-9">
           According to the Pennsylvania System of School Assessment, 34.2% of Philly students in grades 3-8 scored proficient or above on English Language Arts in 2023-24 — the same percentage as in 2022-23. Statewide, students’ English Language Arts proficiency decreased from 54.5% in 2022-23 to 53.9% in 2023-24. Test scores, however, don’t tell a whole story. Across the city, parents, reading specialists, teachers, advocates, and other community members work hard to support children’s learning needs, but many do not have the capacity to solve for multiple factors that are consequences of poverty—and certainly not for every student. Maroon Strategy uses a one-on-one approach and taps into the cultural strengths and pride from which Black students in Philly come. 
