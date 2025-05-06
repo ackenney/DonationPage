fb25174f3b5b8d7e2f2cfa6efeea5015b041ee48
ackenney <ackenney@gmail.com>
2025-05-06 15:00:53 -0400

    fix

--- src/FundApplication/form/AppFormValidation.ts
+++ src/FundApplication/form/AppFormValidation.ts
@@ -1,5 +1,5 @@
 const nameValidationPattern = /^[a-zA-Z.\- ]+$/;
-const dateValidationPattern = /^\d{4}-\d{2}-\d{2}$/;
+const dateValidationPattern = /^\d{4}-\d{2}-\d{2}$/; 
 const emailValidationPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 const addressValidationPattern = /^[a-zA-Z0-9.,\- ]+$/;
 const numberValidationPattern = /^[\+]?[0-9]{0,3}\W?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
